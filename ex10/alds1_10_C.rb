d2423ebed19a86410a612d79479ed3d55d291afc
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-01-18 13:41:58 +0900

    10-C: Array.new is slow

--- ex10/alds1_10_C.rb
+++ ex10/alds1_10_C.rb
@@ -1,15 +1,15 @@
 n = gets.to_i
+memo_mut = Array.new(1001){Array.new(1001){0}}
 n.times do
     q1 = gets.chomp.split("")
     q2 = gets.chomp.split("")
-    memo_mut = Array.new(q1.length + 1){Array.new(q2.length + 1).map{0}}
 
     q1.each.with_index(1){|c1, idx_1|
         q2.each.with_index(1){|c2, idx_2|
             if c1 == c2 then
                 memo_mut[idx_1][idx_2] = memo_mut[idx_1 - 1][idx_2 - 1] + 1
             else
-                memo_mut[idx_1][idx_2] = [memo_mut[idx_1][idx_2 - 1], memo_mut[idx_1 - 1][idx_2]].max
+                memo_mut[idx_1][idx_2] = (tmp1 = memo_mut[idx_1][idx_2 - 1]) > (tmp2 = memo_mut[idx_1 - 1][idx_2]) ? tmp1 : tmp2
             end
         }
     }
